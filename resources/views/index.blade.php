a1deaae49546e1d4bc323f92729909f55ce2fac9
Tay Kong Yu <kongyutay@gmail.com>
2024-12-28 23:44:50 +0800

    create html template and make it inheritance

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,14 +1,17 @@
-<div>
-    The list of tasks
-</div>
+@extends('layouts.app')
 
-@forelse ($tasks as $task)
-    <div>
-        <a href="{{ route('tasks.show', ['id' => $task->id ]) }}">
-            {{ $task -> title }}
-        </a>
-    </div>
+{{-- section 的第二个参数接收一个默认值，如果里面没有提供内容，就会使用默认值，以下就是例子 --}}
+@section('title', 'The list of task')
 
-@empty
-    <div>There are no tasks!</div>
-@endforelse
+@section('content')
+    @forelse ($tasks as $task)
+        <div>
+            <a href="{{ route('tasks.show', ['id' => $task->id]) }}">
+                {{ $task->title }}
+            </a>
+        </div>
+
+    @empty
+        <div>There are no tasks!</div>
+    @endforelse
+@endsection
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,9 +1,15 @@
-<h1>{{ $task->title }}</h1>
-<p>{{ $task->description}}</p>
+@extends('layouts.app')
 
-@if ($task->long_description)
-    <p>{{$task->long_description}}</p>
-@endif
+{{-- section 的第二个参数接收一个默认值，如果里面没有提供内容，就会使用默认值，以下就是例子 --}}
+@section('title', $task->title)
 
-<p>{{$task->created_at}}</p>
-<p>{{$task->updated_at}}</p>
+@section('content')
+    <p>{{ $task->description }}</p>
+
+    @if ($task->long_description)
+        <p>{{ $task->long_description }}</p>
+    @endif
+
+    <p>{{ $task->created_at }}</p>
+    <p>{{ $task->updated_at }}</p>
+@endsection
