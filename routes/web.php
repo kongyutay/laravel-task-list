01154633343cd9979a900145444349c691a115ac
Tay Kong Yu <kongyutay@gmail.com>
2024-12-28 16:16:17 +0800

    basic routing

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Main page';
+    // return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return 'hello' ;
+});
+
+Route::get('/greet/{name}', function ($name) {
+    return 'hello ' . $name . '!'; 
 });
