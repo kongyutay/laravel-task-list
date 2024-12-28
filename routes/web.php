36ed773882e1f6a2b9099f3f8186384cf3df2aa9
Tay Kong Yu <kongyutay@gmail.com>
2024-12-28 16:37:49 +0800

    redirect and fallback

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,20 @@ Route::get('/', function () {
 
 Route::get('/hello', function () {
     return 'hello' ;
+})->name('hello');
+
+// Redirect routing
+Route::get('/hallo', function () {
+    // redirect funciton returns an object so may direct access something inside
+    // return redirect('/hello') ;
+    return redirect()->route('hello') ;
 });
 
 Route::get('/greet/{name}', function ($name) {
-    return 'hello ' . $name . '!'; 
+    return 'hello ' . $name . '!';
+});
+
+// 虽然内建了404页面，但是这个可以依然带去这个地方
+Route::fallback(action: function () {
+    return 'Still got somewhere!' ;
 });
