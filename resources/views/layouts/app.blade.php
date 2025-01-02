2be9de4db8924d00bf0f8992bdae591455e8c9d8
Tay Kong Yu <kongyutay@gmail.com>
2025-01-02 22:30:03 +0800

    flash message using alpinejs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel 11 Task List App</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     {{-- blade-formatter-disable --}}
     <style type="text/tailwindcss">
         .btn {
@@ -34,10 +35,27 @@
 
 <body class="container mx-auto mt-10 max-w-lg">
     <h1 class="text-2xl mb-4">@yield('title')</h1>
-    <div>
+    {{-- set initial value of flash variable --}}
+    <div x-data="{ flash: true }">
         @if (session()->has('success'))
-            <div>{{ session('success') }}</div>
+            <div x-show="flash"
+                class="relative mb-10 rounded border border-green-400 bg-green-100 px-4 py-3 text-lg text-green-700"
+                role="alert">
+                <strong class="font-bold">Success!</strong>
+                <div>
+                    {{ session('success') }}
+                </div>
+
+                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                    {{-- here will set the flash to the false --}}
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        @click="flash = false" stroke="currentColor" class="h-6 w-6 cursor-pointer">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </span>
+            </div>
         @endif
+
         @yield('content')
     </div>
 </body>
