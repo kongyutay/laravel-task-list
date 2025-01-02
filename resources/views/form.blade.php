efccdd8b000c176009c1eedcdff4f83d46b6bc97
Tay Kong Yu <kongyutay@gmail.com>
2025-01-02 17:02:36 +0800

    fixed bug

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -14,7 +14,7 @@
 @section('content')
     {{-- 如果有返回错误，会是error对象，具体可以读取errors对象内的属性 --}}
     {{-- {{ $errors }} --}}
-    <form action="POST" action="{{ isset($task) ? route('tasks.update', ['task' => $task->id]) : route('tasks.store') }}">
+    <form method="POST" action="{{ isset($task) ? route('tasks.update', ['task' => $task->id]) : route('tasks.store') }}">
         {{-- 有了这个csrf，laravel会通过middleware自动验证token是否存在于表单之中 --}}
         {{-- session会放在storage/framework/sessions里面的一个文件，这是默认设置，如果规模变大，无法share这些session去其他server，所以可以在session.php文件那里调整设置参数，把driver中的file调成redis或者memcached，这才是最好的方案 --}}
         @csrf
@@ -33,7 +33,7 @@
 
         <div>
             <label for="description">Description</label>
-            <textarea name="description" id="description" rows="5">{{ $task->description ?? old('title') }}</textarea>
+            <textarea name="description" id="description" rows="5">{{ $task->description ?? old('description') }}</textarea>
             @error('description')
                 <p class="error-message">{{ $message }}</p>
             @enderror
@@ -41,7 +41,7 @@
 
         <div>
             <label for="long_description">Long Description</label>
-            <textarea name="long_description" id="long_description" rows="10">{{ $task->long_description ?? old('title') }}</textarea>
+            <textarea name="long_description" id="long_description" rows="10">{{ $task->long_description ?? old('long_description') }}</textarea>
             @error('long_description')
                 <p class="error-message">{{ $message }}</p>
             @enderror
